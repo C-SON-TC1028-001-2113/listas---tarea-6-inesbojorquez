c6e04586f6f3d90c5b5a1f72b4d65f2232f67e35
inesbojorquez <a01252378@itesm.mx>
2021-10-06 20:07:12 +0000

    09SerieFibonacciListas

--- assignments/09SerieFibonacciListas/src/exercise.py
+++ assignments/09SerieFibonacciListas/src/exercise.py
@@ -1,4 +1,21 @@
 def main():
+    lista = []
+    n = -1
+    while n<0:
+        n = int(input())
+
+    if n==0:
+        print("[]")
+    elif n>0:
+        lista = [0,1]
+        if n>2:
+            for i in range(n-2):
+                lista.append(lista[i]+lista[i+1])
+            print(lista)
+        elif n==1:
+            print("[0]")
+        elif n==2:
+            print(lista)
     
     pass
 
