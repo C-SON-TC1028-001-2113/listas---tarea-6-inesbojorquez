6b57d4aa08afcf3c5a17334bb286f04ca0b8c9c7
inesbojorquez <a01252378@itesm.mx>
2021-10-06 19:49:50 +0000

    17MenoresANumero

--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,6 +1,25 @@
-
-
+def resultados(m):
+    listafinal=[]
+    for i in range(len(m)):
+        if m[i]<10:
+            listafinal.append(m[i])
+    return listafinal
+def datos(a,b):
+    valores=[]
+    matriz=[]
+    listacompleta=[]
+    for i in range(a):
+        for i in range(b):    
+            valor=int(input())
+            valores.append(valor)
+        matriz.append(valores)
+        listacompleta=listacompleta+valores
+        valores=[]
+    return resultados(listacompleta)
 def main():
+    renglones=int(input())
+    columnas=int(input())
+    print (datos(renglones,columnas))
    
 
 
